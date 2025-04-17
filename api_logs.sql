212735e0fb076f9c500651d0b98e73dec442e789
greeshmadh <greezdh@gmail.com>
2025-04-17 15:58:17 +0530

    all functionalities

--- api_logs.sql
+++ api_logs.sql
@@ -13,10 +13,43 @@ CREATE TABLE IF NOT EXISTS api_logs (
     timestamp DATETIME
 );
 
-SELECT endpoint, COUNT(*) AS request_count
-FROM api_logs
-GROUP BY endpoint
-ORDER BY request_count DESC;
+SELECT 
+    endpoint, 
+    COUNT(*) AS request_count
+FROM 
+    api_logs
+GROUP BY 
+    endpoint
+ORDER BY 
+    request_count DESC;
+
+
+
+SELECT 
+    DATE_FORMAT(timestamp, '%Y-%m-%d %H:00:00') AS hour_slot,
+    AVG(response_time_ms) AS avg_response_time
+FROM 
+    api_logs
+GROUP BY 
+    hour_slot
+ORDER BY 
+    hour_slot;
+
+
+
+SELECT 
+    endpoint, 
+    status_code, 
+    COUNT(*) AS error_count
+FROM 
+    api_logs
+WHERE 
+    status_code >= 400
+GROUP BY 
+    endpoint, status_code
+ORDER BY 
+    error_count DESC;
+
 
 truncate table api_logs;
 select * from api_logs;
